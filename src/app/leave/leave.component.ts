8230a0b508909c112f3265dfd253fdb377a346fa
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-12-09 00:36:02 +0600

    Leave application done with proper validators

--- src/app/leave/leave.component.ts
+++ src/app/leave/leave.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { LeaveService } from '../all_services/leave.service';
 import { CreateLeave } from '../config/interfaces/leave.interface';
 import { LeaveCategoryService } from '../all_services/leave-category.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material';
+import { DialogLeaveConfirmationComponent } from '../dialogs/dialog-leave-confirmation/dialog-leave-confirmation.component';
 
 @Component({
   selector: 'app-leave',
@@ -10,31 +13,75 @@ import { LeaveCategoryService } from '../all_services/leave-category.service';
 })
 export class LeaveComponent implements OnInit {
 
-  public leaveCategories;
+  leaveCategories: any;
+  leaveApplicationForm: FormGroup;
+  errMessage: any;
 
   constructor(
     private leaveService: LeaveService,
-    private leaveCategoryService: LeaveCategoryService
+    private leaveCategoryService: LeaveCategoryService,
+    private formBuilder: FormBuilder,
+    private dialog: MatDialog,
   ) { }
 
   ngOnInit() {
     this.leaveCategoryService.getLeaveCategories().subscribe(res => {
       this.leaveCategories = res[0].leave_categories;
     });
+    this.buildForm();
   }
 
   applyForLeave() {
     const leaveApplication: CreateLeave = {
-      leave_category_id: (document.getElementById('category') as HTMLInputElement).value,
-      leave_description: (document.getElementById('description') as HTMLInputElement).value,
-      start_date: (document.getElementById('start_date') as HTMLInputElement).value,
-      end_date: (document.getElementById('end_date') as HTMLInputElement).value,
+      leave_category_id: this.leaveApplicationForm.value.leaveCategoryId,
+      leave_description: this.leaveApplicationForm.value.description,
+      start_date: this.convertDatePickerToString(this.leaveApplicationForm.value.startDate),
+      end_date: this.convertDatePickerToString(this.leaveApplicationForm.value.endDate),
     };
 
-    this.leaveService.submitLeaveApplication(leaveApplication).subscribe(response => {
-      console.log(response);
-      this.checkError(response[0]);
+    this.dialog.open(DialogLeaveConfirmationComponent, {
+      data: {
+        message: 'Request For Leave',
+      }
+    }).afterClosed().subscribe(digResponse => {
+        if (digResponse.toString() === '1') {
+          this.leaveService.submitLeaveApplication(leaveApplication).subscribe(response => {
+            if (! this.checkError(response[0])) {
+              alert('Leave application submitted succesfully');
+            }
+          });
+        }
     });
+  }
+
+  convertDatePickerToString(paramDate: any) {
+    if (! paramDate) {
+      return null;
+    }
+
+    let stringDate: string;
+    stringDate = `${paramDate.year}-${paramDate.month}-${paramDate.day}` as string;
+    return stringDate;
+  }
+
+  validateTwoDates() {
+    let start: any = this.convertDatePickerToString(this.startDate.value);
+    start = new Date(start).getTime();
+    let end: any = this.convertDatePickerToString(this.endDate.value);
+    end = new Date(end).getTime();
+    const now = new Date().getTime();
+    const tolerance = 86400 * 1000;
+
+    if (start > 0 && end > 0 && start > end) {
+      this.errMessage = 'Start date can\'t be greater than the End date.';
+      return false;
+    } else if (start > 0 && end > 0 && ((now - start) >= tolerance || (now - end) >= tolerance)) {
+      this.errMessage = 'Start date or End date can\'t be smaler than today.';
+      return false;
+    } else {
+      this.errMessage = null;
+      return true;
+    }
   }
 
   private checkError(response: any) {
@@ -43,6 +90,53 @@ export class LeaveComponent implements OnInit {
       return true;
     }
     return false;
+  }
+
+  buildForm() {
+    this.leaveApplicationForm = this.formBuilder.group({
+      leaveCategoryId: ['', [
+          Validators.required,
+        ],
+      ],
+
+      startDate: ['', [
+          Validators.required,
+        ],
+      ],
+
+      endDate: ['', [
+          Validators.required,
+        ],
+      ],
+
+      description: ['', [
+          Validators.required,
+        ],
+      ],
+    });
+  }
+
+  get leaveCategoryId() {
+    return this.leaveApplicationForm.get('leaveCategoryId');
+  }
+
+  get startDate() {
+    return this.leaveApplicationForm.get('startDate');
+  }
+
+  get endDate() {
+    return this.leaveApplicationForm.get('endDate');
+  }
+
+  get description() {
+    return this.leaveApplicationForm.get('description');
+  }
+
+  checkOverAllBeforeLogin(credentials: any) {
+    // console.log(credentials);
+    this.leaveApplicationForm.setErrors({
+      invalidLogin: true
+    });
   }
 
 }

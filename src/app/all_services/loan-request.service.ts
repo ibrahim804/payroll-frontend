00babf8175d10e0dee4a626476bcd5bd1dfc38ad
Ibrahim Khalil <bsse0804@iit.du.ac.bd>
2019-12-12 20:34:00 +0600

    Refactored

--- src/app/all_services/loan-request.service.ts
+++ src/app/all_services/loan-request.service.ts
@@ -10,6 +10,10 @@ export class LoanRequestService {
 
   constructor(private authService: AuthService) { }
 
+  getActualPF_OnLoan_AvailablePF() {
+    return this.authService.getFromHTTP(apiRoutes.userMe);
+  }
+
   requestForLoan(data: Create) {
     return this.authService.postInHTTP(apiRoutes.loanRequest, data);
   }
--- src/app/apply-loan-request/apply-loan-request.component.ts
+++ src/app/apply-loan-request/apply-loan-request.component.ts
@@ -1,4 +1,7 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { LoanRequestService } from './../all_services/loan-request.service';
 import { Component, OnInit } from '@angular/core';
+import { CustomValidators } from '../shared/custom.validators';
 
 @Component({
   selector: 'app-apply-loan-request',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApplyLoanRequestComponent implements OnInit {
 
-  constructor() { }
+  myConsiderableAmounts: any;
+  loanRequestForm: FormGroup;
+
+  constructor(
+    private loanRequestService: LoanRequestService,
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit() {
+    this.buildForm();
+    this.loanRequestService.getActualPF_OnLoan_AvailablePF().subscribe(response => {
+      this.myConsiderableAmounts = response[0].description;
+      // console.log(this.myConsiderableAmounts);
+    });
+  }
+
+  buildForm() {
+    this.loanRequestForm = this.formBuilder.group({
+        // name: ['', [
+        //     Validators.required,
+        //     Validators.minLength(3),
+        //     Validators.maxLength(25),
+        //     CustomValidators.containsOnlyAlphabet,
+        //   ],
+        // ],
+      requested_amount: ['', [
+          Validators.required,
+          Validators.pattern(/^\d+$/),
+          Validators.min(0),
+        ],
+      ],
+    });
+  }
+
+  get requested_amount() {
+    return this.loanRequestForm.get('requested_amount');
   }
 
 }
